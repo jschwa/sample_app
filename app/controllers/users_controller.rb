5fa635fccfcd300e199239836fd5eea98ab0a04c
jschwa <Jason@new-host-2.home>
2013-02-06 22:06:50 -0500

    Finish user singup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,21 @@
 class UsersController < ApplicationController
-  def new
-  end
+  
+	def show
+  		@user = User.find(params[:id])
+	end
+
+	def new
+  		@user = User.new
+	end
+
+	def create
+		@user = User.new(params[:user])
+		if @user.save
+			flash[:sucess] = "Welcome to the Sample App!"
+			redirect_to @user
+			#handle a succesful save.
+		else
+			render 'new'
+		end
+	end
 end
---
+++ spec/factories.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+	factory :user do
+		name "Jason Schwartz"
+		email "jshwaz@gmail.com"
+		password "foobar"
+		password_confirmation "foobar"
+	end
+end
+
